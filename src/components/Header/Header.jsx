065c850dec4223590fb6f1f323ee2bbe84a7c33d
joaocdfarias <joaocdfariass@gmail.com>
2021-12-06 23:50:37 -0300

    style(component): add responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 
 export const Header = () => {
+  const [dropdown, setDropdown] = useState(false)
+
+  const handleClick = () => {
+    setDropdown(!dropdown)
+  }
+
   return (
     <header>
       <a className="logo" href="/">
         DevChallenge
       </a>
       <nav>
-        <ul>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/features">Features</a>
-          </li>
-          <li>
-            <a href="contact">Contact</a>
-          </li>
-        </ul>
+        <button className="burger-menu" onClick={handleClick}>
+          ≣
+        </button>
+        {dropdown && (
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/features">Features</a>
+            </li>
+            <li>
+              <a href="contact">Contact</a>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   )
